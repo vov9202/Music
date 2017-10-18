e92e2e8806262985859a05ff072a0a98e0ed788c
USER <USER@USER15-PC>
2017-10-18 18:59:13 +0900

    뺄셈 함수 추가

--- NWTest/src/Calculator.java
+++ NWTest/src/Calculator.java
@@ -1,5 +1,11 @@
 //초기파일
 
 public class Calculator {
+	public int snum(int num1, int num2) {
+		return num1 + num2;
+	}
 	
+	public int substraction(int num1, int num2) {
+		return num1 - num2;
+	}
 }
