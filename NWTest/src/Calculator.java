1cc7323e85c5a2ddbd00f0e0f51841afb7072e38
USER <USER@USER15-PC>
2017-10-19 19:16:36 +0900

    1916 cal

--- NWTest/src/Calculator.java
+++ NWTest/src/Calculator.java
@@ -9,11 +9,6 @@ public class Calculator {
 		return num1 - num2;
 	}
 	
-	public int multiply(int num1, int num2) {
-		return num1 * num2;
-	}
 	
-	public int division(int num1, int num2) {
-		return num1 / num2;
-	}
+	
 }
