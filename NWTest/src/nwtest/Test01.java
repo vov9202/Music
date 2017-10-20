fe7338409145b75d1a6bddef2104cbc7fd65428b
USER <USER@USER15-PC>
2017-10-20 17:41:10 +0900

    학원 컴퓨터 작업 1741

---
+++ NWTest/src/nwtest/Test01.java
@@ -0,0 +1,26 @@
+//학원 컴퓨터 작업111111111
+
+package nwtest;
+
+public class Test01 {
+	private String name;
+	private String tel;
+	private String job;
+	private String content;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+}
